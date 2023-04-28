8f488577b5125972a4001c0e0947eebc87c3a790
Belgarde Jean Ronel <belganel@gmail.com>
2023-04-29 01:47:11 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-// import { dictionaryApi } from "./api.js";
+import { dictionaryApi } from "./api.js";
 import { theme } from "./theme.js";
 
-// document.addEventListener('DOMContentLoaded', dictionaryApi.init);
+document.addEventListener('DOMContentLoaded', dictionaryApi.init);
 
 document.addEventListener("DOMContentLoaded", theme.init);
 
