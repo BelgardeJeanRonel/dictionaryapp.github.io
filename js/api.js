11514bb4dd7c4b2c961953381f51716414793f30
Belgarde Jean Ronel <belganel@gmail.com>
2023-04-29 16:40:46 +0200

    update

--- js/api.js
+++ js/api.js
@@ -1,42 +1,42 @@
-// import { createElement, createTemplate } from "./_functions.js";
+import { createTemplate, createElement } from "./_functions.js";
 
-function createElement (tagName) {
-    const element = document.createElement(tagName);
+// function createElement (tagName) {
+//     const element = document.createElement(tagName);
   
-    return element;
-  }
+//     return element;
+// }
 
-function createTemplate() {
-    document.querySelector(".section__word").innerHTML = `
-        <div>
-          <h1 class="section__word--title"></h1>
-          <span class="section__word--phonetic"></span>
-        </div>
+// function createTemplate() {
+//     document.querySelector(".section__word").innerHTML = `
+//         <div>
+//           <h1 class="section__word--title"></h1>
+//           <span class="section__word--phonetic"></span>
+//         </div>
         
-        <div class="section__word--audio">
-          <svg class="icon-play">
-            <use xlink:href="svg/SpritePlay.svg#play"></use>
-          </svg>
-        </div>`;
-    document.querySelector(".section__noun").innerHTML = `
-    <h2></h2>
+//         <div class="section__word--audio">
+//           <svg class="icon-play">
+//             <use xlink:href="svg/SpritePlay.svg#play"></use>
+//           </svg>
+//         </div>`;
+//     document.querySelector(".section__noun").innerHTML = `
+//     <h2></h2>
 
-        <h3></h3>
-        <ul>
+//         <h3></h3>
+//         <ul>
 
-        </ul>
+//         </ul>
 
-        <div class="synonym"><span></span> <span></span></div>`;
-    document.querySelector(".section__verb").innerHTML = `
-    <h2></h2>
+//         <div class="synonym"><span></span> <span></span></div>`;
+//     document.querySelector(".section__verb").innerHTML = `
+//     <h2></h2>
 
-        <h3></h3>
-        <ul>
+//         <h3></h3>
+//         <ul>
 
-        </ul>`;
-    document.querySelector(".source").innerHTML = `
-    <span></span><a href="" target="_blank"> <i class="fa fa-external-link"></i></a>`;
-}
+//         </ul>`;
+//     document.querySelector(".source").innerHTML = `
+//     <span></span><a href="" target="_blank"> <i class="fa fa-external-link"></i></a>`;
+// }
 
 
 async function fetchJSON(url, options = {}) {
@@ -77,6 +77,8 @@ export const dictionaryApi = {
 
                 if (inputUser) {
                     createTemplate();
+
+                    console.log(createTemplate);
 
                     const wordTitle = document.querySelector(".section__word--title");
                     const phonetic = document.querySelector(".section__word--phonetic");
@@ -137,6 +139,7 @@ export const dictionaryApi = {
                     for (const def of nounMeaning) {
                     
                         const newLiElement = createElement("li");
+                        console.log(newLiElement);
                         newLiElement.textContent = def.definition;
             
                         ulNounElement.append(newLiElement);
