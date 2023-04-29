b70fbfcdd94b6e1bc0c7d9d4a4d71a81c8591c53
Belgarde Jean Ronel <belganel@gmail.com>
2023-04-29 16:59:29 +0200

    update

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-import { createElement } from "./_functions.js";
+import { createElement, createTemplate } from "./functions.js";
 
 // function createElement (tagName) {
 //     const element = document.createElement(tagName);
@@ -6,37 +6,37 @@ import { createElement } from "./_functions.js";
 //     return element;
 // }
 
-function createTemplate() {
-    document.querySelector(".section__word").innerHTML = `
-        <div>
-          <h1 class="section__word--title"></h1>
-          <span class="section__word--phonetic"></span>
-        </div>
+// function createTemplate() {
+//     document.querySelector(".section__word").innerHTML = `
+//         <div>
+//           <h1 class="section__word--title"></h1>
+//           <span class="section__word--phonetic"></span>
+//         </div>
         
-        <div class="section__word--audio">
-          <svg class="icon-play">
-            <use xlink:href="svg/SpritePlay.svg#play"></use>
-          </svg>
-        </div>`;
-    document.querySelector(".section__noun").innerHTML = `
-    <h2></h2>
+//         <div class="section__word--audio">
+//           <svg class="icon-play">
+//             <use xlink:href="svg/SpritePlay.svg#play"></use>
+//           </svg>
+//         </div>`;
+//     document.querySelector(".section__noun").innerHTML = `
+//     <h2></h2>
 
-        <h3></h3>
-        <ul>
+//         <h3></h3>
+//         <ul>
 
-        </ul>
+//         </ul>
 
-        <div class="synonym"><span></span> <span></span></div>`;
-    document.querySelector(".section__verb").innerHTML = `
-    <h2></h2>
+//         <div class="synonym"><span></span> <span></span></div>`;
+//     document.querySelector(".section__verb").innerHTML = `
+//     <h2></h2>
 
-        <h3></h3>
-        <ul>
+//         <h3></h3>
+//         <ul>
 
-        </ul>`;
-    document.querySelector(".source").innerHTML = `
-    <span></span><a href="" target="_blank"> <i class="fa fa-external-link"></i></a>`;
-}
+//         </ul>`;
+//     document.querySelector(".source").innerHTML = `
+//     <span></span><a href="" target="_blank"> <i class="fa fa-external-link"></i></a>`;
+// }
 
 
 async function fetchJSON(url, options = {}) {
